27eaa055377be96509ab993708d870e1044e1346
Francesco Donnarumma <pagondonnarumma@gmail.com>
2022-12-08 18:47:35 +0100

    creazione del progetto maven

---
+++ FamtaTextGame/src/main/java/com/fantatextgame/FamtaTextGame/App.java
@@ -0,0 +1,13 @@
+package com.fantatextgame.FamtaTextGame;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
