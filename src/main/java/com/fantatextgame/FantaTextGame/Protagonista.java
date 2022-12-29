fbb90c54415a9fa15c9b1047e5fce7451ccfe3f9
FrancescoDonnarumma2005 <pagondonnarumma@gmail.com>
2022-12-29 17:02:35 +0100

    risposte personalizzate rispettive alle scelte del giocatore
    risposte personalizzate rispettive alle scelte del giocatore e aggiornamento della javadoc

--- src/main/java/com/fantatextgame/FantaTextGame/Protagonista.java
+++ src/main/java/com/fantatextgame/FantaTextGame/Protagonista.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
  *	@author Giuseppe Liberato Serra
  *	@author Giuseppe Borzachiello
  *	@author Kevin Gargiulo
- *	@version 1.3.6
+ *	@version 1.9.6
 **/
 public class Protagonista {
 	
@@ -20,19 +20,22 @@ public class Protagonista {
 		private int numLivelloAttuale=0;
 		private String titolo;
 		private String[] scenari=new String[5]; //Array di stringhe che contiene le descrizioni con le scelte dei vari scenari 
-		private int[] CorrectAnswer=new int[5]; //array di stringhe che contiene il numero corrispondente alla scelta corretta
+		private int[] correctChoises=new int[5]; //array di stringhe che contiene il numero corrispondente alla scelta corretta
 		private String trama;
-		//TODO variabile che contiene la trama 
+		private String[][] answer=new String[5][4];
+		
 		
 		
 		/**costruttore che lancia i metodi 
-		 *{@link Protagonista#setScenari() setScenari()}, {@link Protagonista#setCorrectAnswer() setCorrectAnswer()}, {@link Protagonista#setTitolo() setTitolo()} e {@link Protagonista#setTrama() setTrama()}*/
+		 *{@link Protagonista#setScenari() setScenari()}, {@link Protagonista#setCorrectChoise() setCorrectAnswer()}, {@link Protagonista#setTitolo() setTitolo()}, {@link Protagonista#setTrama() setTrama()}
+		 *e {@link Protagonista#setAnswer() setAnswer()}*/
 		public Protagonista(){
 			
 			this.setScenari();
-			this.setCorrectAnswer(); 
+			this.setCorrectChoise(); 
 			this.setTitolo();
 			this.setTrama();
+			this.setAnswer();
 			
 		}
 		/**Setta in un array di stringhe i vari scenari corrispondenti ai vari livelli */
@@ -61,7 +64,7 @@ public class Protagonista {
 					+ "conficcandosi in un punto specifico nel terreno. Cosa fai?\n\n" + 
 					"1. Abbandona la pala per ricongiungerti con i tuoi compagni\n" + 
 					"2. Inizia a scavare utilizzando la pala\n" + 
-					"3. Prendi la pala per tornare dai tuoi compagni con la pala per potergli spiegare cos'è successo\n" + 
+					"3. Prendi la pala per tornare dai tuoi compagni e spiegare loro cos'è successo\n" + 
 					"4. Usa la spada per rompere la pala\n" + 
 					"";
 			scenari[4]="Ti ritrovi una decina di metri più in basso rispetto a dove hai iniziato a scavare e hai davanti a te un anello.\n"
@@ -76,27 +79,28 @@ public class Protagonista {
 					"";
 		}
 		
-		/**Stampa la stringa dello scenario rispettivo al livello */
-	    private void getScenari() {
-	       System.out.println(scenari[numLivelloAttuale-1]); 
+		/**ritorna la stringa dello scenario rispettivo al livello attuale
+		 *@return la stringa dello scenario rispettivo al livello attuale  */
+	    private String getScenari() {
+	       return scenari[numLivelloAttuale-1]; 
 	    }
 		
 		
 		/** un get che ritorna a seconda del livello attuale la risposta corretta corrispondente a quel livello 
 		 * @return la risposta corretta rispettiva allo scenario */
 		
-		private int getCorrectAnswer() {
-			return CorrectAnswer[numLivelloAttuale-1];
+		private int getCorrectChoise() {
+			return correctChoises[numLivelloAttuale-1];
 		}
 		
 		
 		/**setta il numero corrispondente alla scelta giusta */
-				private void setCorrectAnswer() {
-			        CorrectAnswer[0]=3;
-			        CorrectAnswer[1]=3;
-			        CorrectAnswer[2]=1;
-			        CorrectAnswer[3]=2;
-			        CorrectAnswer[4]=4; 
+				private void setCorrectChoise() {
+			        correctChoises[0]=3;
+			        correctChoises[1]=3;
+			        correctChoises[2]=1;
+			        correctChoises[3]=2;
+			        correctChoises[4]=4; 
 			    }
 				
 		/**funzione che stampa il menu e la trama se il livello è 0 o se il gioco è stato restartato 
@@ -116,22 +120,55 @@ public class Protagonista {
 		public boolean isRestarted() {
 			return isRestarted;
 		}
-
 		
-		
-		
-		/**funzione che controlla se la scelta relativa allo scenario è corretta
+		/**setta la risposta relativa alle scelte prese dal giocatore riguardanti i vari scenari*/
+		private void setAnswer() {
+			answer[0][0]="I ricchi ti hanno subito notato provare a scappare ed hanno ucciso te e la tua famiglia";
+			answer[0][1]="I ragazzi appartenenti a Muller ti ridono in faccia e senza troppi movimenti uccidono te e tutti i tuoi compagni";
+			answer[0][2]="Tu e i tuoi compagni avete distrutto tutti i ragazzi di Muller. Complimenti!";
+			answer[0][3]="In questa parte della voragine non c'è nessuna roccia con cui nasconderti e i ragazzi di muller ti hanno ucciso";
+			answer[1][0]="Il serpente nonostante una testa mozzata non sembra aver ricevuto alcun danno e ti ha ucciso";
+			answer[1][1]="La tua spada non è arrivata al cuore del serpente. Il serpente ha approfittato della situazione di stallo e ti ha ucciso";
+			answer[1][2]="Sei scappato, per ora non c'è traccia del serpente. Sei scampato di nuovo alla morte complimenti!";
+			answer[1][3]="I serpenti riescono a vederti perfettamente anche se sei immobile. Sei stato ucciso!";
+			answer[2][0]="Complimenti bel lancio!! Hai tagliato perfettamente tutte e 3 le teste e lo hai finalmente ucciso";
+			answer[2][1]="Il serpente ha completamente ignorato il tuo zaino e ti ha ucciso";
+			answer[2][2]="La tua spada è rimasta bloccata tra le carni del serpente e sei stato ucciso";
+			answer[2][3]="Sei in un vicolo cieco, il tuo tentativo di fuga è stato inutile e ti sei fatto ammazzare";
+			answer[3][0]="Durante il cammino per ricongiungerti con i tuoi amici, dei ragazzi appartenenti ad un altro ricco ti hanno preso di mira e ti hanno ucciso";
+			answer[3][1]="Incredibile! riesci a scavare con una facilità e una velocità mai viste";
+			answer[3][2]="Questa volta nessun ascia! Durante il cammino per ricongiungerti con i tuoi amici, dei ragazzi appartenenti ad un altro ricco ti hanno preso di mira e ti hanno ucciso";
+			answer[3][3]="La pala non si è rotta ma il tuo attacco ti si è ritorto contro e sei stato ucciso";
+			answer[4][0]="Il ricco a cui appartenevi ha notato il tuo comportamento ribelle e un raggio provenente dall'alto della voragine ti ha ucciso";
+			answer[4][1]="Non ci è voluto molto per far si che arrivassero tutte le altre persone nei dintorni e che iniziassero una lotta nel buco che hai scavato.\n"
+					+ "Sei rimasto coinvolto dalla lotta e sei stato ucciso";
+			answer[4][2]="Sei stato ucciso. Il ricco a cui appartieni gioisce e preso dall'euforia, non ha impiegato molto tempo prima di iniziare la distruzione della voragine\n"
+					+ " e di tutti i ragazzi coinvolti";
+			answer[4][3]="Tutti i ragazzi sono stati trasportati immediatamente fuori dalla voragine, assistendo ad un mondo completamente nuovo.\n"
+					+ "Il dislivello tra ricchi e poveri è stato colmato, le famiglie più povere messe in salvo e al posto della voragine ora c'è un immenso prato fiorito dalla bellezza\n"
+					+ "indescrivibile, quasi come se di tutta questa avventura, non fosse mai esistito nulla...";
+		}
+		/**ritorna la stringa della risposta rispettiva alla scelta e al livello attuale
 		 * @param scelta numero relativo alla scelta dello scenario a schermo
-		 * @throws InvalidValueForGameScene lancia un eccezione se la scelta non è compresa tra 1 e 4*/
+		 * @return la stringa della risposta rispettiva alla scelta e al livello attuale*/
+		private String getAnswer(int scelta) {
+			return answer[numLivelloAttuale-1][scelta-1];
+		}
+		
+		
+		/**funzione che controlla se la scelta relativa allo scenario è corretta e stampa una risposta relativa scelta del giocatore
+		 * @param scelta numero relativo alla scelta dello scenario a schermo
+		 * @throws InvalidValueForGameScene lancia un eccezione se la scelta non è compresa tra 1 e 4
+		 * @see Protagonista#getAnswer(scelta)*/
 		public void isCorrectChoise(int scelta) throws InvalidValueForGameScene {
-	        if (scelta==this.getCorrectAnswer()) {
-	        	System.out.println("BRAVO CE L’HAI FATTA!");
+	        if (scelta==this.getCorrectChoise()) {
+	        	System.out.println(this.getAnswer(scelta));
 	        }
 	        else if(scelta>4 && scelta<1) {
 	        	throw new InvalidValueForGameScene();
 	        }
 	        else {
-	            System.out.println("HAI PERSO, RISPOSTA SBAGLIATA, SEI STATO ANNIENTATO!");
+	        	System.out.println(this.getAnswer(scelta));
 	            System.exit(0);
 	        }
 	    }
@@ -150,7 +187,7 @@ public class Protagonista {
 		
 		public void GameController(int scelta) throws InvalidValueForGameMenu {
 	        switch (scelta) {
-	        case 1: numLivelloAttuale++; this.getScenari(); isRestarted=false; break;
+	        case 1: numLivelloAttuale++; System.out.println(this.getScenari()); isRestarted=false; break;
 	        case 2: System.out.println("HAI PERSO PERCHE’ TI SEI RITIRATO"); System.exit(0); break;
 	        case 3: numLivelloAttuale=0; isRestarted=true; break;
 	        default: throw new InvalidValueForGameMenu(); 
